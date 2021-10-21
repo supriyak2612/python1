2dc2fdf180dba50fb32684309cc80eb661e8abb6
supriyak2612 <supriya.karekar@wipro.com>
2021-10-21 18:14:05 +0530

    updated

---
+++ Hello/contacts.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# ContactBook/contacts.py
+
+"""This module provides RP Contacts entry point script."""
+
+from contacts.main import main
+
+if __name__ == "__main__":
+	main()
+    
---
+++ Hello/contacts/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# contacts/__init__.py
+
+"""This module provides the contacts package."""
+
+__version__ = "0.1.0"
